3d4a5aad284ef45e986b9197f955deeb243e140f
idhrenniel <miguel.angel.munoz.tubio@est.fib.upc.edu>
2017-03-29 19:58:44 +0200

    es dirigido o no?

---
+++ proyectoIA1/src/proyecto/ia/pair.java
@@ -0,0 +1,14 @@
+package proyecto.ia;
+
+/**
+ * Created by idhrenniel on 29/03/17.
+ */
+public class pair {
+    public int x;
+    public int y;
+
+    public pair(int xx, int yy){
+        x = xx;
+        y = yy;
+    }
+}
