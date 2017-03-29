46cec5e6904429b156dbf9700c750866d5619ce9
idhrenniel <miguel.angel.munoz.tubio@est.fib.upc.edu>
2017-03-29 19:59:50 +0200

    dirigido?

--- proyectoIA1/src/proyecto/ia/EstadoProblema.java
+++ proyectoIA1/src/proyecto/ia/EstadoProblema.java
@@ -1,18 +1,19 @@
 package proyecto.ia;
 
-import IA.Red.CentrosDatos;
-import IA.Red.Sensores;
-
+import IA.Red.*;
 import java.util.Scanner;
+import java.util.*;
 
 /**
  * Created by idhrenniel on 24/03/17.
  */
 public class EstadoProblema {
 
-    private Sensores s;
-    private CentrosDatos c;
+    private Sensores ss;
+    private CentrosDatos cs;
 
+    HashMap<Sensor, ArrayList<pair>> sensorMap = new HashMap<>();
+    HashMap<Centro, ArrayList<pair>> centerMap = new HashMap<>();
 
     EstadoProblema(){
         Scanner sc = new Scanner(System.in);
@@ -31,17 +32,76 @@ public class EstadoProblema {
             nCentros = sc.nextInt();
         }
 
-        System.out.println("Entre su semilla, guapo.");
-        int seed     = sc.nextInt();
-        while(seed < 0){
-            System.out.println("El numero de la semilla no puede ser negativo.");
-            seed     = sc.nextInt();
+        System.out.println("Entre dos semillas diferentes, guapo.");
+        int seed1   = sc.nextInt();
+        int seed2   = sc.nextInt();
+
+        while(seed1 == seed2){
+            System.out.println("Las semillas no pueden ser iguales. Introduzca dos de nuevo.");
+            seed1   = sc.nextInt();
+            seed2   = sc.nextInt();
         }
 
-        s = new Sensores(nSensors, seed);
-        c = new CentrosDatos(nCentros, seed);
+        ss = new Sensores(nSensors, seed1);
+        cs = new CentrosDatos(nCentros, seed2);
+
+        for(int i = 0; i != ss.size(); ++i){
+            Sensor s = ss.get(i);
+            ArrayList<pair> conexiones = new ArrayList<pair>();
+            sensorMap.put(s,conexiones);
+        }
+
+        for(int i=0; i != cs.size(); ++i){
+            Centro c = cs.get(i);
+            ArrayList<pair> conexiones = new ArrayList<pair>();
+            centerMap.put(c, conexiones);
+        }
+
+        Output();
+
     }
 
+    void Output(){
+        System.out.println("SENSORES");
+        for(Sensor key: sensorMap.keySet()){
+            System.out.println(key.getCoordX()+" "+key.getCoordY());
+            conectarSS(key, key);
+        }
 
+        System.out.println("CENTROS");
+        for(Centro key: centerMap.keySet()){
+            System.out.println(key.getCoordX()+" "+key.getCoordY());
+        }
+    }
+
+    //OPERADORES
+
+    public void conectarSS(Sensor s1, Sensor s2) {
+        pair p = new pair(s2.getCoordX(), s2.getCoordY());
+        sensorMap.get(s1).add(p);
+        p.x = s1.getCoordX(); p.y = s1.getCoordY();
+        sensorMap.get(s2).add(p);
+    }
+
+    public void conectarSC(Sensor s, Centro c) {
+        pair p = new pair(c.getCoordX(), c.getCoordY());
+        sensorMap.get(s).add(p);
+        p.x = s.getCoordX(); p.y = s.getCoordY();
+        centerMap.get(c).add(p);
+    }
+
+    public void desconectarSS(Sensor s1, Sensor s2){
+        pair p = new pair(s2.getCoordX(), s2.getCoordY());
+        sensorMap.get(s1).remove(p);
+        p.x = s1.getCoordX(); p.y = s1.getCoordY();
+        sensorMap.get(s2).remove(p);
+    }
+
+    public void desconectarSC(Sensor s, Centro c){
+        pair p = new pair(c.getCoordX(), c.getCoordY());
+        sensorMap.get(s).remove(p);
+        p.x = s.getCoordX(); p.y = s.getCoordY();
+        centerMap.get(c).remove(p);
+    }
 
 }
