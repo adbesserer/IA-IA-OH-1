4caf42ecfc11c9219432795cbaf5ef4a88c24d61
idhrenniel <miguel.angel.munoz.tubio@est.fib.upc.edu>
2017-04-01 16:08:21 +0200

    Operadores funcionando al 100% y funcion de compute_volume implementada (llamarla garantiza que los volumenes son correctos) y Hill Climbing / Simulated Annealing realizadas en clase. Falta observar si funcionan.

--- proyectoIA1/src/proyecto/ia/HillClimbing.java
+++ proyectoIA1/src/proyecto/ia/HillClimbing.java
@@ -1,6 +1,5 @@
 package proyecto.ia;
 
-import java.lang.reflect.Array;
 import java.util.*;
 
 /**
--- proyectoIA1/src/proyecto/ia/SimulatedAnnealing.java
+++ proyectoIA1/src/proyecto/ia/SimulatedAnnealing.java
@@ -1,15 +1,35 @@
 package proyecto.ia;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 /**
  * Created by idhrenniel on 1/04/17.
  */
 public class SimulatedAnnealing {
 
-    private double temp = 10000;
+    private double temp = 10000.0;
     private double coolingRate = 0.003;
 
-    public EstadoProblema getBestSolution(){
-        return null;
+    private double acceptanceProbability(double stateH, double newStateH){
+        if(newStateH < stateH) return 1.0;
+        return Math.exp((stateH - newStateH) / this.temp);
+    }
+
+    public EstadoProblema getBestSolution(EstadoProblema state){
+        SuccesorFunction sf = new SuccesorFunction();
+        while(temp > 1){
+            List retval = sf.getSuccessors(state);
+
+            EstadoProblema newState = ((ArrayList<EstadoProblema>) retval).get(0);
+            if (acceptanceProbability(state.coste_total(), newState.coste_total()) > Math.random()) {
+                if(state.coste_total() > newState.coste_total()) state = newState;
+            }
+
+            this.temp *= 1-coolingRate;
+        }
+
+        return state;
     }
 
     public void setTemp(double newTemp) {
