842f295c2a19b2614fe70663b4858198551088a8
Adri96 <adriamunuera@me.com>
2017-04-03 12:06:52 +0200

    Exp3

---
+++ proyectoIA1/src/proyecto/ia/Experimento3.java
@@ -0,0 +1,28 @@
+package proyecto.ia;
+
+/**
+ * Created by adriamunuera on 3/4/17.
+ */
+public class Experimento3 {
+    private final static int nsensores = 100;
+    private final static int ncentros = 4;
+    private EstadoProblema ep;
+
+
+    public Experimento2() {
+        ep = new EstadoProblema(nsensores, ncentros);
+        ep.generar_sol_ini_1();
+        System.out.println("CONFIGURACIÓN INICIAL:");
+        ep.showconnections();
+    }
+
+    public void testGen1(){     //comprobar los resultados del primer generador de soluciones iniciales
+
+    }
+    public void testGen2(){     //comprobar los resultados del segundo generador de soluciones iniciales
+
+    }
+    public void testGen3(){     //comprobar los resultados del tercer generador de soluciones iniciales
+
+    }
+}
