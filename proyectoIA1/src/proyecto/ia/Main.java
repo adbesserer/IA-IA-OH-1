7706da83ec975167b00825cc0aad2854c41d312a
alejandro.dominguez.besserer <alejandro.dominguez.besserer@est.fib.upc.edu>
2017-04-03 13:31:29 +0200

    Creador con copia

--- proyectoIA1/src/proyecto/ia/Main.java
+++ proyectoIA1/src/proyecto/ia/Main.java
@@ -9,47 +9,20 @@ public class Main {
         ep.generar_sol_ini_3();
         double costeInicial = ep.coste_total();
         int volumenInicial = ep.volumen_total();
-        Scanner sc = new Scanner(System.in);
-        String command;
-        while(true) {
-            System.out.println("List of Volumes going through the nodes: ");
-            ep.compute_volumes();
-            ep.showconnections();
-            System.out.println("Total cost: " + ep.coste_total());
-            System.out.println("Total volume: " + ep.volumen_total() + "Mb/s");
-            //System.out.println("Write a command (e.g. change or switch");
-            //command = new String(sc.next());
-            /*if(command.equals("change")) {
-                System.out.println("Introduce la clave del sensor y la del nuevo destino del cable");
-                Integer k1 = new Integer(sc.nextInt());
-                Integer k2 = new Integer(sc.nextInt());
-                ep.changecable(k1, k2);
-            }
-            else if(command.equals("switch")){
-                System.out.println("Introduce las claves de los sensores cuyos cables quieres intercambiar");
-                Integer k1 = new Integer(sc.nextInt());
-                Integer k2 = new Integer(sc.nextInt());
-                ep.switchcables(k1,k2);
-            }*/
+        System.out.println("List of Volumes going through the nodes: ");
 
-            ////////////////////////////////////////////////////////////////////////////
-            //MAMASO Y EL CARBASO lol xd lmao tse tung
-            ////////////////////////////////////////////////////////////////////////////
-            //System.out.println("1. HILL CLIMBING\n2. SIMULATED ANNEALING");
-            HillClimbing hc = new HillClimbing();
-            ep = hc.getBestSolution(ep);
+        ep.compute_volumes();
+        ep.showconnections();
+        System.out.println("Total cost: " + ep.coste_total());
+        System.out.println("Total volume: " + ep.volumen_total() + "Mb/s");
 
-            System.out.println("\n\n\nAQUI ESTA");
-            ep.Output();
-
-            ep.showconnections();
-
-            double costeFinal = ep.coste_total();
-            int volumenFinal = ep.volumen_total();
-
-            System.out.println("Costes antes y después: " +costeInicial+ " "+ costeFinal+
+        HillClimbing hc = new HillClimbing();
+        ep = hc.getBestSolution(ep);
+        ep.Output();
+        ep.showconnections();
+        double costeFinal = ep.coste_total();
+        int volumenFinal = ep.volumen_total();
+        System.out.println("Costes antes y después: " +costeInicial+ " "+ costeFinal+
                                 "\nVolumenes antes y después: " + volumenInicial +" " +  volumenFinal);
-            while(true);
-        }
     }
 }
