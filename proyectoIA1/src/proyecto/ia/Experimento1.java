6abcb6f88152491d1b629747dec13ff5af46afc4
alejandro.dominguez.besserer <alejandro.dominguez.besserer@est.fib.upc.edu>
2017-04-03 16:42:40 +0200

    KAIBA YOU'RE SWITCHIN' FOR A BRUISIN'
    LET'S D-D-D-D-D-D-DUEL!

--- proyectoIA1/src/proyecto/ia/Experimento1.java
+++ proyectoIA1/src/proyecto/ia/Experimento1.java
@@ -15,24 +15,53 @@ public class Experimento1 {
     private final static int nsensores = 100;
     private final static int ncentros = 4;
     private EstadoProblema ep;
-
+    private double coste_inicial, coste_final;
+    private int volumen_inicial, volumen_final;
 
     public Experimento1() {
         ep = new EstadoProblema(nsensores, ncentros);
         ep.generar_sol_ini_1();
         System.out.println("CONFIGURACIÓN INICIAL:");
         ep.showconnections();
+        ep.compute_volumes();
+        coste_inicial   = ep.coste_total();
+        volumen_inicial = ep.volumen_total();
     }
 
     public void testSwitch(){       // comprobar los resultados usando solo el operador switchcables
+        HillClimbing hc = new HillClimbing();
+        EstadoProblema best = hc.getBestSolution(new EstadoProblema(ep), true, false); //switch= true, change = false
+        volumen_final = best.volumen_total();
+        coste_final   = best.coste_total();
 
+        System.out.println("La mejora en coste usando sólo el operador de switch cables es :"
+                        + (coste_final-coste_inicial));
+        System.out.println("La diferencia en volumen es" + (volumen_final-volumen_inicial));
     }
     public void testChange(){       // comprobar los resultados usando solo el operador change cable
+        HillClimbing hc = new HillClimbing();
+        EstadoProblema best = hc.getBestSolution(new EstadoProblema(ep), false, true); //switch= false, change = true
+        volumen_final = best.volumen_total();
+        coste_final   = best.coste_total();
 
+        System.out.println("La mejora en coste usando sólo el operador de switch cables es :"
+                + (coste_final-coste_inicial));
+        System.out.println("La diferencia en volumen es" + (volumen_final-volumen_inicial));
     }
     public void testBoth(){         // comprobar los resultados usando ambos operadores: esperamos el mejor rendimiento aqui
+        HillClimbing hc = new HillClimbing();
+        EstadoProblema best = hc.getBestSolution(new EstadoProblema(ep), true, true); //switch= true, change = true
+        volumen_final = best.volumen_total();
+        coste_final   = best.coste_total();
 
+        System.out.println("La mejora en coste usando sólo el operador de switch cables es :"
+                + (coste_final-coste_inicial));
+        System.out.println("La diferencia en volumen es" + (volumen_final-volumen_inicial));
     }
-
+    public void run(){ // corre los tres tests
+        testChange();
+        testSwitch();
+        testBoth();
+    }
 
 }
